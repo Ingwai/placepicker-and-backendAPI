f20208f5b0fb94cd1f462b44e32db00f249ec4da
Ingwai <ingwai@wp.pl>
2023-12-23 16:30:56 +0100

    add custom hook useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { fetchUserPlaces, updateUserPlaces } from './http.js';
+import { updateUserPlaces, fetchUserPlaces } from './http.js';
 import Error from './components/Error.jsx';
+import { useFetch } from './hooks/useFetch.js';
 
 function App() {
 	const selectedPlace = useRef();
 
-	const [userPlaces, setUserPlaces] = useState([]);
 	const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
-	const [isFetching, setIsFetching] = useState(false);
 	const [modalIsOpen, setModalIsOpen] = useState(false);
-	const [error, setError] = useState();
 
-	useEffect(() => {
-		const fetchPlaces = async () => {
-			setIsFetching(true);
-
-			try {
-				const places = await fetchUserPlaces();
-				setUserPlaces(places);
-			} catch (error) {
-				setError({ message: error.message || 'Failed to fetch user places' });
-			}
-			setIsFetching(false);
-		};
-		fetchPlaces();
-	}, []);
-
-	if (error) {
-		return (
-			<Error
-				title='An error occured!'
-				message={error.message || 'Could not fetch places, please try again later.'}></Error>
-		);
-	}
+	const { fetchedData: userPlaces, isFetching, error, setFetchedData: setUserPlaces } = useFetch(fetchUserPlaces, []);
 
 	function handleStartRemovePlace(place) {
 		setModalIsOpen(true);
@@ -80,7 +57,7 @@ function App() {
 			}
 			setModalIsOpen(false);
 		},
-		[userPlaces]
+		[userPlaces, setUserPlaces]
 	);
 
 	const handleError = () => {
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,40 +1,22 @@
-import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
+
+const fetchSortedPlaces = async () => {
+	const places = await fetchAvailablePlaces();
+
+	return new Promise((res, rej) => {
+		navigator.geolocation.getCurrentPosition(position => {
+			const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
+			res(sortedPlaces);
+		});
+	});
+};
 
 export default function AvailablePlaces({ onSelectPlace }) {
-	const [availablePlaces, setAvailablePlaces] = useState([]);
-	const [isFetching, setIsFetching] = useState(false);
-	const [error, setError] = useState();
-
-	useEffect(() => {
-		// fetch('http://localhost:3000/places')
-		// 	.then(response => response.json())
-		// 	.then(resData => {
-		// 		setAvailablePlaces(resData.places);
-		// 	});
-
-		const fetchPlaces = async () => {
-			setIsFetching(true);
-
-			try {
-				const places = await fetchAvailablePlaces();
-				// wyznaczanie lokalizacji użytkownika i sotowanie miejsc ze względu na odległość
-				navigator.geolocation.getCurrentPosition(position => {
-					const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
-					setAvailablePlaces(sortedPlaces);
-					setIsFetching(false); //wykonuje się tuż po pobraniu posortowanej tablicy miejsc
-				});
-			} catch (error) {
-				setError({ message: error.message });
-				setIsFetching(false); // gdy wyświetli jakiś błąd to też kończymy wyświetlanie informacji o pobieraniu danych
-			}
-			// to zostawiamy poza blokiem try catch bo ma być false niezależnie czy juz zakończy się pobierania czy wyrzuci jakiś błąd
-		};
-		fetchPlaces();
-	}, []);
+	const { isFetching, error, fetchedData: availablePlaces } = useFetch(fetchSortedPlaces, []);
 
 	if (error) {
 		return (
